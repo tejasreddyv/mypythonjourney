0a782a789a1ccccadb49f47157f77b4546029168
tejasreddyv <tejasreddyv123@gmail.com>
2025-02-26 10:23:16 +0530

    My First Program

---
+++ First Program.py
@@ -0,0 +1,54 @@
+print("Tejas Reddy V")
+print(75/22)
+print("Amma,Appa,Me")
+Amma="Understanding"
+Appa="Brave"
+Me="Love God"
+print(Appa,Amma,Me)
+Amma="Godess"
+Appa="God"
+Me="Son"
+print(Me,Appa,Amma)
+print(type(Amma),type(Appa),type(Me)) #Printing The Type
+A=77#Printing Sum
+B=66
+Sum=A+B
+print(Sum)
+#Arithametic Operations
+print(A**B)
+print(A%B)# % here is Not Percentage Instead The Modular Symbol (remainder when divided by two integers)
+#Relational Operators (used to find out the relation between two variables by python)
+print(A>=B)#True
+print(A!=B)#True
+print(A<=B)#False
+print(A>B)#True (note:- Always Returns A Boolean Value I.E TRUE OR FALSE)
+#Assignment Operators
+Num=66
+Num/=99#Etc HAVE BEEN TRIED BUT I AM LAZY TO WRITE THEM
+print(Num)
+#logical operators (only Applicable to boolean values)
+print(not(A<B))#true
+print(A==B and A>B)#false
+print(A!=B or A<=B)#true
+#type Casting Consisting Of 2 types 1] Automatic (Done by interprator) 2]Manual(Forcefully Done By Us)
+#Eg For Manual IS Given Below
+A=int("60")
+B=6.9
+print(A+B)#Now 60 Will be Converted into Float type Because float is Superior than Integer
+#Manual Coversion Done By Us Now i Will Covert 60 Which i Have Made A string Into Integer VAlue
+print(A+B)
+#Taking Input From The User
+Name=(input("Please Enter Your Name:-"))
+Movie=(input("What Is Your Favourate Movie:-"))
+Reason=(input("Why Is It So That It Is Your Favourate Movie:-"))
+print("So Hello",Name)
+print(Movie,"Is A Good Choice I Like It Too")
+print(Reason,"HA!That's So Nice Of You,Hope You Have A Good Day")
+#Practice Time Boyzzzz!(1Question)
+A=int(input ("Enter The First Number:-"))
+B=int(input("Enter The Second Number:-"))
+print("So The Sum Of These Numbers Is",A+B)
+#2 Question
+Num1=float(input("The First One:-"))
+Num2=float(input("The second one;-"))
+print(Num1>Num2)
